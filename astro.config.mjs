e82ca2107cec233aabef0a90f5e13b46eb19250b
Guillaume Lecêtre <guillaume.lecetre@aworldforus.com>
2024-05-26 17:34:32 +0200

    add GH Pages deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://glecetre.github.io",
+  base: "cosmos-aria",
   integrations: [tailwind()],
 });
